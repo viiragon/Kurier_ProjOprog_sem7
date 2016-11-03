66fe1bb9c8ece669084b38e12a154a258e61f9da
Marcin Adamczyk <marcin.m.adamczyk@gmail.com>
2016-11-03 12:24:28 +0100

    Widok - interfejsy - CentralaSamochody i CentralaPaczki

---
+++ Kurier/Views/IVCentralaPaczki.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestAPSNET.Views
+{
+    interface IVCentralaPaczki
+    {
+        void wyswietlFormularzEdycjiStatusuPaczki(String[] statusy);
+        void wyswietlOknoListyPaczek(Models.DanePaczki[] paczki);
+        void wyswietlOknoSzczegolowPaczki(Models.DanePaczki paczka);
+    }
+}
---
+++ Kurier/Views/IVCentralaSamochody.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAPSNET.Views
+{
+    interface IVCentralaSamochody
+    {
+        void aktualizujOknoSzczegolowSamochodu(Models.DaneSamochodu samochod);
+        void wyswietlOknoDodawaniaSamochodu();
+        void wyswietlOknoListySamochodow(Models.DaneSamochodu[] lista);
+        void wyswietlOknoSzczegolowSamochodu(Models.DaneSamochodu samochod);
+        void wyswietlOknoWysylaniaZleceniaDoSerwisu(Models.DaneSamochodu samochod);
+    }
+}
