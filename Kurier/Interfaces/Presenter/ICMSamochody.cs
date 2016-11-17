6f1259651c96b413ab24eae7e4106cff49bef928
Marcin Adamczyk <marcin.m.adamczyk@gmail.com>
2016-11-17 14:20:08 +0100

    Dodanie struktury Widoku -> CentralaSamochody v2

---
+++ Kurier/Interfaces/Presenter/ICMSamochody.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kurier.Interfaces.Presenter
+{
+    public abstract class ICMSamochody
+    {
+        public abstract void wybranoAktualizujSzczegolySamochodu(int id);
+        public abstract void wybranoDodajSamochod();
+        public abstract void wybranoOtworzFormularzZleceniaDoSerwisu(int id);
+        public abstract void wybranoPokazListeSamochodow();
+        public abstract void wybranoPokazSzczegolySamochodu(int id);
+        public abstract void wybranoPrzypiszKurieraDoSamochodu(int idSamochodu);
+        public abstract void wybranoUsunSamochod(int id);
+        public abstract void wybranoWyslijZlecenieDoSerwisu();
+        public abstract void wybranoZapiszNowySamochod(Models.DTO.Samochod.DaneSamochodu dane);
+        public abstract void wybranoZapiszPowiazanieKurieraZSamochodem(int idKuriera);
+    }
+}
---
+++ Kurier/Views/Menu/CentralaSamochody.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CentralaSamochody.aspx.cs" Inherits="Kurier.Views.Menu.CentralaSamochody" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h1>CENTRALA SAMOCHODY</h1>
+        <h3>Dodaj nowy samochód</h3>
+        <p>
+            Nr rejestracyjny: <asp:TextBox runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Stan: <asp:TextBox runat="server"></asp:TextBox>
+        </p>
+        <p>
+            Kurier: <asp:TextBox runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button Text="Dodaj nowy samochód" OnClick="onClickBtAddCar" runat="server"/>
+        </p>
+    </div>
+    </form>
+</body>
+</html>
