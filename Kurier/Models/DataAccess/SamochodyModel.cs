d0167c94c73b634f51a02cc0f900b81fee6f8ebb
Jacek Krzemiński <jacek.krzeminski@interia.pl>
2016-12-01 15:31:16 +0100

    testy modelu + dbInit

--- Kurier/Models/DataAccess/SamochodyModel.cs
+++ Kurier/Models/DataAccess/SamochodyModel.cs
@@ -10,62 +10,62 @@ using Kurier.Models.DTO.Uzytkownik;
 
 namespace Kurier.Models.DataAccess
 {
-  public class SamochodyModel : IMSamochody
-  {
-    public void DodajSamochod(DaneSamochodu samochod)
+    public class SamochodyModel : IMSamochody
     {
-      using (var db = new ApplicationContext())
-      {
-        db.Samochody.Add(samochod);
-        db.SaveChanges();
-      }
+        public void DodajSamochod(DaneSamochodu samochod)
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.Samochody.Add(samochod);
+                db.SaveChanges();
+            }
+        }
+
+        public List<DaneSamochodu> PobierzListeSamochodow()
+        {
+            using (var db = new ApplicationContext())
+            {
+                return db.Samochody.ToList();
+            }
+        }
+
+        public DaneSamochodu PobierzSamochod(int id)
+        {
+            using (var db = new ApplicationContext())
+            {
+                return db.Samochody.Find(id);
+            }
+        }
+
+        public void PowiazKurieraISamochod(int idSamochodu, int idKuriera)
+        {
+
+            using (var db = new ApplicationContext())
+            {
+                DaneKuriera kurier = db.Kurierzy.FirstOrDefault(p => p.UserId == idKuriera);
+                DaneSamochodu samochod = db.Samochody.Find(idSamochodu);
+
+                if (samochod != null)
+                    kurier.Samochod = samochod;
+
+                db.SaveChanges();
+            }
+
+        }
+
+        public void UsunSamochod(int id)
+        {
+            using (var db = new ApplicationContext())
+            {
+                var samochod = db.Samochody.Find(id);
+                db.Samochody.Remove(samochod);
+                db.SaveChanges();
+            }
+        }
+
+        public bool WalidujDaneSamochodu(DaneSamochodu samochod)
+        {
+            throw new NotImplementedException();
+        }
     }
-
-    public List<DaneSamochodu> PobierzListeSamochodow()
-    {
-      using (var db = new ApplicationContext())
-      {
-        return db.Samochody.ToList();
-      }
-    }
-
-    public DaneSamochodu PobierzSamochod(int id)
-    {
-      using (var db = new ApplicationContext())
-      {
-        return db.Samochody.Find(id);
-      }
-    }
-
-    public void PowiazKurieraISamochod(int idSamochodu, int idKuriera)
-    {
-
-      using (var db = new ApplicationContext())
-      {
-        DaneKuriera kurier=db.Kurierzy.FirstOrDefault(p => p.UserId == idKuriera);
-        DaneSamochodu samochod = db.Samochody.Find(idSamochodu);
-
-        if (samochod != null)
-          kurier.Samochod= samochod;
-
-        db.SaveChanges();
-      }
-
-    }
-
-    public void UsunSamochod(int id)
-    {
-      using (var db = new ApplicationContext())
-      {
-        var samochod = db.Samochody.Find(id);
-        db.Samochody.Remove(samochod);
-        db.SaveChanges();
-      }
-    }
-
-    public bool WalidujDaneSamochodu(DaneSamochodu samochod)
-    {
-      throw new NotImplementedException();
-    }
-  }
 }
---
+++ KurierTest/Models/IMStatystykaTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KurierTest.Models
+{
+    [TestClass]
+    public class IMStatystykaTest
+    {
+        [TestMethod]
+        public void PobierzNajczestszeObszaryPaczekTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PobierzObciazenieKurierowTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PobierzStatystykeNajczestszychKlientowTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void PobierzStatystykiPaczekTest()
+        {
+
+        }
+    }
+}
