18e7c78db2cf67755f329a24169f6576ff555239
Mateusz <Mateusz@DESKTOP-5EAFBD3>
2016-12-07 20:18:02 +0100

    SamochodyPrezenter - dodawanie, usuwanie, aktualizowanie, lista, szczegóły

--- Kurier/Presenters/CentralaManagera/SamochodyCM/SamochodyPrezenter.cs
+++ Kurier/Presenters/CentralaManagera/SamochodyCM/SamochodyPrezenter.cs
@@ -2,10 +2,81 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Kurier.Interfaces.Presenter;
+using Kurier.Models.DTO.Samochod;
+using Kurier.Models.DataAccess;
 
 namespace Kurier.Presenters.CentralaManager.SamochodyCM
 {
-    public class SamochodyPrezenter
+    public class SamochodyPrezenter : ICMSamochody
     {
+        private Interfaces.View.IVCentralaSamochody samochody;
+        private SamochodyModel samochodyModel;
+        public SamochodyPrezenter()
+        {
+            samochody = Interfaces.View.IVCentralaSamochody.createInstance(this);
+            samochodyModel = new SamochodyModel();
+        }
+
+        public void wybranoAktualizujSzczegolySamochodu(int id)
+        {
+            DaneSamochodu samochod = samochodyModel.PobierzSamochod(id);
+            samochody.aktualizujOknoSzczegolowSamochodu(samochod);
+        }
+
+        public void wybranoDodajSamochod()
+        {
+            samochody.wyswietlOknoDodawaniaSamochodu();
+        }
+
+        public void wybranoOtworzFormularzZleceniaDoSerwisu(int id)
+        {
+            DaneSamochodu samochod = samochodyModel.PobierzSamochod(id);
+            samochody.wyswietlOknoWysylaniaZleceniaDoSerwisu(samochod);
+        }
+
+        public void wybranoPokazListeSamochodow()
+        {
+            DaneSamochodu[] listaSamochodow = samochodyModel.PobierzListeSamochodow().ToArray();
+            samochody.wyswietlOknoListySamochodow(listaSamochodow);
+        }
+
+        public void wybranoPokazSzczegolySamochodu(int id)
+        {
+            DaneSamochodu samochod = samochodyModel.PobierzSamochod(id);
+            samochody.wyswietlOknoSzczegolowSamochodu(samochod);
+        }
+
+        public void wybranoPrzypiszKurieraDoSamochodu(int idSamochodu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void wybranoUsunSamochod(int id)
+        {
+            samochodyModel.UsunSamochod(id);
+            wybranoPokazListeSamochodow();
+        }
+
+        public void wybranoWyslijZlecenieDoSerwisu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void wybranoZapiszNowySamochod(DaneSamochodu dane)
+        {
+            bool czyPoprawneDane = samochodyModel.WalidujDaneSamochodu(dane);
+            if (czyPoprawneDane)
+            {
+                samochodyModel.DodajSamochod(dane);
+                //komunikat o dodaniu
+            }
+            wybranoPokazListeSamochodow();
+        }
+
+        public void wybranoZapiszPowiazanieKurieraZSamochodem(int idKuriera)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
