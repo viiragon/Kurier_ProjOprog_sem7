5813fb53b431e0a2da01fec0163954c0d3315c9d
Radosław Guzek <radoslaw.guzek@gmail.com>
2016-12-18 23:59:41 +0100

    Testy połączenia z bazą danych

---
+++ KurierTest/Models/DatabaseConnectionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.Common;
+using Kurier.Models.Context;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KurierTest.Models
+{
+  [TestClass]
+  public class DatabaseConnectionTest
+  {
+    [TestMethod]
+    public void TestConnection()
+    {
+      using (var db = new ApplicationContext())
+      {
+        DbConnection conn = db.Database.Connection;
+        try
+        {
+          conn.Open();   // check the database connection
+        }
+        catch
+        {
+          throw new AssertFailedException();
+        }
+        Assert.IsTrue(db.Database.Exists());
+
+      }
+    }
+  }
+}
