f7aeaa528b3fc2486bb2ab176db2db7e6a02c95b
Mateusz <Mateusz@DESKTOP-5EAFBD3>
2017-01-10 19:23:45 +0100

    StatystykiPrezenter

---
+++ Kurier/Models/DataAccess/StatystykiModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Kurier.Interfaces.Model;
+using Kurier.Models.DTO.Statystyka;
+
+namespace Kurier.Models.DataAccess
+{
+    public class StatystykiModel : IMStatystyka
+    {
+        public StatystykaObszaru PobierzNajczestszeObszaryPaczek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ObciazenieKurierow PobierzObciazenieKurierow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StatystykaKlientow PobierzStatystykeNajczestszychKlientow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StatystykaPaczek PobierzStatystykiPaczek()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
