11f1452ca3a7c5dccb8acc8b03b19a7a0df9745a
Artur <stpnav@gmail.com>
2017-01-18 23:09:23 +0100

    Migrate data

---
+++ Kurier/Migrations/201701182206525_2.cs
@@ -0,0 +1,24 @@
+namespace Kurier.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _2 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.DaneSamochodus", "Kurier_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.DaneSamochodus", new[] { "Kurier_Id" });
+            DropColumn("dbo.DaneSamochodus", "Discriminator");
+            DropColumn("dbo.DaneSamochodus", "Kurier_Id");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.DaneSamochodus", "Kurier_Id", c => c.String(maxLength: 128));
+            AddColumn("dbo.DaneSamochodus", "Discriminator", c => c.String(nullable: false, maxLength: 128));
+            CreateIndex("dbo.DaneSamochodus", "Kurier_Id");
+            AddForeignKey("dbo.DaneSamochodus", "Kurier_Id", "dbo.AspNetUsers", "Id");
+        }
+    }
+}
